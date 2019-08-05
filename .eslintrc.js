0e868cf69b554418b1ba23f8aa4fb08f455c04ba
Terry Yu <tyu@ebates.com>
2019-08-05 10:22:12 -0700

    Custom rule of max-len and no-use-before-define

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,16 @@ module.exports = {
     'comma-dangle': [ERROR, 'always-multiline'],
     quotes: [ERROR, 'single'],
     strict: [ERROR, 'never'],
+    'max-len': [ERROR, { 
+      'code': 120,
+      // sometimes we are providing a json response sample in the comment which is too long
+      'ignoreComments': true,
+      // certain url could be pretty long
+      'ignoreStrings': true,
+      // similar to the string above
+      'ignoreTemplateLiterals': true
+    }],
+    // to allow helper functions to be defined below the core function to get an overview of the feature quickly
+    'no-use-before-define': [ERROR, { 'functions': false }],
   }
 }
