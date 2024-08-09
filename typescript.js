b35e99fc5222c42f03e64a168fcf5ec6601a865e
Jimmy Multani <jimmy.multani@rakuten.com>
2024-08-09 12:59:40 -0400

    comment out

--- typescript.js
+++ typescript.js
@@ -7,18 +7,18 @@ const { WARNING } = require('./constants');
 module.exports = {
   // allows ESLint to read TS syntax
   parser: '@typescript-eslint/parser',
-  plugins: [
-    // allows TS rule usage
-    '@typescript-eslint',
-  ],
-  // extends: [
-  //   // disables a few of the recommended rules from the previous set that we know are already covered by TypeScript's typechecker
-  //   'plugin:@typescript-eslint/eslint-recommended',
-  //   // turns on rules from their TypeScript-specific plugin
-  //   'plugin:@typescript-eslint/recommended',
-  //   // enables ts/tsx file usage when importing modules
-  //   'plugin:import/typescript',
+  // plugins: [
+  //   // allows TS rule usage
+  //   '@typescript-eslint',
   // ],
+  extends: [
+    // disables a few of the recommended rules from the previous set that we know are already covered by TypeScript's typechecker
+    'plugin:@typescript-eslint/eslint-recommended',
+    // turns on rules from their TypeScript-specific plugin
+    'plugin:@typescript-eslint/recommended',
+    // enables ts/tsx file usage when importing modules
+    'plugin:import/typescript',
+  ],
   rules: {
     // Allow TypeScript files to have JSX
     'react/jsx-filename-extension': [
