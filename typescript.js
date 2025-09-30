9c915803b634b35e2cb10223de5e523bf71fa212
Jimmy Multani <jimmy.multani@rakuten.com>
2025-09-29 22:28:36 -0400

    fix: streamline TypeScript ESLint configuration by removing unused plugins and parser options

--- typescript.js
+++ typescript.js
@@ -2,24 +2,16 @@
  * This file contains the rules for TypeScript.
  */
 // const { OFF, ERROR, WARNING } = require('./constants');
-const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
-const typescriptParser = require('@typescript-eslint/parser');
+
+const tseslint = require('typescript-eslint');
 const importPlugin = require('eslint-plugin-import');
 
 /** @type {import('eslint').Linter.Config[]} */
 module.exports = [
-  // we need to spread this config as it is an array and not an object
-  ...typescriptPlugin.configs['flat/recommended'],
+  tseslint.configs.recommended,
   importPlugin.flatConfigs.typescript,
   {
     files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        // allows TS syntax
-        sourceType: 'module',
-      },
-    },
     rules: {},
   },
 ];
