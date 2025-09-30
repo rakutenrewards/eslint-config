38a0eed0b8ae84a5912c8fb22d3a9f40298c32b1
Jimmy Multani <jimmy.multani@rakuten.com>
2025-09-29 22:33:46 -0400

    fix: enhance TypeScript ESLint configuration by spreading recommended rules and adding specific rules for JavaScript files

--- typescript.js
+++ typescript.js
@@ -1,15 +1,21 @@
 /**
  * This file contains the rules for TypeScript.
  */
-// const { OFF, ERROR, WARNING } = require('./constants');
+const { OFF } = require('./constants');
 
 const tseslint = require('typescript-eslint');
 const importPlugin = require('eslint-plugin-import');
 
 /** @type {import('eslint').Linter.Config[]} */
 module.exports = [
-  tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
   importPlugin.flatConfigs.typescript,
+  {
+    files: ['**/*.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': OFF,
+    },
+  },
   {
     files: ['**/*.ts', '**/*.tsx'],
     rules: {},
